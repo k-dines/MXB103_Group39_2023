6d4fa43316d66aa63b8178cd41012e01890b72cb
k-dines <52302579+k-dines@users.noreply.github.com>
2023-10-28 01:18:38 +1000

    Add files via upload

---
+++ camera_points.m
@@ -0,0 +1,28 @@
+function [tCamera, yCamera] = camera_points(t, y, Camera)
+y1 =[];
+y2= [];
+t1= [];
+t2 =[];
+
+reached = false;
+lengthT = size(t);
+
+
+for i = 1:lengthT(2)
+    if y(i) >= Camera
+        reached = true;
+    end
+
+    if ~reached
+        y1 = [y1, y(i)];
+        t1 = [t1, t(i)];
+    else
+        y2 = [y2, y(i)];
+        t2 = [t2, t(i)];
+    end   
+
+end
+tCamera = [t1(end-1), t1(end), t2(1), t2(2)];
+yCamera = [y1(end-1), y1(end), y2(2), y2(1)];
+
+end
